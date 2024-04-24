19a3031346b9173cf002d99cda27e360fb40761b
SamiraMarasy <samiramarassy@gmail.com>
2024-04-24 19:42:29 +0200

    project creation

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Samira Marassy on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
